2ad4969d17051b38d9bd0b7a9d6c64b2e0e7c521
Rodrigo Bettio <rodriguinhobettiojr1@gmail.com>
2025-04-29 17:07:15 -0300

    Re-adiciona a pasta frontend como diretório normal

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Exemplo from './components/Exemplo';
+
+function App() {
+  return (
+    <div>
+      <h2>Conexão Django + React</h2>
+      <Exemplo />
+    </div>
+  );
+}
+
+export default App;
