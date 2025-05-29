07726ef76f0809a41dd3733d4badc7108765a051
Rodrigo Bettio <rodriguinhobettiojr1@gmail.com>
2025-05-28 22:11:11 -0300

    style: Aplicando blur no background quando acessa a modal

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,14 +16,18 @@ function Home() {
       <Sidebar />
       <div className="flex-grow-1" style={{ marginLeft: "280px" }}>
         <Header />
-        <div className="main-content" style={{ marginTop: "10rem", padding: "2rem" }}>
+        <div
+          className={`main-content ${modalAberto ? "blur-background" : ""}`}
+          style={{ marginTop: "10rem", padding: "2rem" }}
+        >
           <div className="d-flex flex-wrap gap-4">
             <CreateProjectCard onClick={handleCreateProject} />
           </div>
         </div>
-        <ModalNewProject isOpen={modalAberto} onClose={() => setModalAberto(false)} />
       </div>
+      <ModalNewProject isOpen={modalAberto} onClose={() => setModalAberto(false)} />
     </div>
+
   );
 }
 
