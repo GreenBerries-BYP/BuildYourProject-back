4d74da0094769512dd3390754313835f0a4d8be5
Letícia Rudeli <leticiarudeli11@gmail.com>
2025-06-10 18:06:11 -0300

    wip: tentando consertar bootstrap, algumas inconsistencias

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import './styles/Main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 // Main.jsx é o ponto de entrada da aplicação React. Ele importa o React e o ReactDOM, além do componente App.
 
