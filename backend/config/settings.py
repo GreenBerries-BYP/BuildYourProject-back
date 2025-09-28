5eb62e8c58d5fadf9c449182d681389a19d0fad3
marisa <marisa.mayumi.mo@gmail.com>
2025-09-28 18:12:21 -0300

    fix: settings para resolver CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -173,6 +173,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
 ]
 
@@ -181,6 +182,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
 ]
 
