e6244d645bb2c4a83396311dbd8e14bf3920423e
marisa <marisa.mayumi.mo@gmail.com>
2025-09-28 19:05:51 -0300

    fix: erro semantico

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -170,7 +170,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://buildyourproject-front.onrender.com",
-    "https://front-buildyourproject-aig3.onrender.com";
+    "https://front-buildyourproject-aig3.onrender.com",
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
@@ -180,7 +180,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://buildyourproject-front.onrender.com",
-    "https://front-buildyourproject-aig3.onrender.com";
+    "https://front-buildyourproject-aig3.onrender.com",
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
